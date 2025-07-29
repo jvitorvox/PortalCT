bff70ab4c070be6ea5bb3d1fd13024373555ccb3
jvitorvox <38590487+jvitorvox@users.noreply.github.com>
2025-07-29 16:35:09 -0300

    Criar estrutura do projeto Portal Casa & Terra

--- src/PortalCT.Web/Program.cs
+++ src/PortalCT.Web/Program.cs
@@ -1,46 +1,12 @@
-using Microsoft.EntityFrameworkCore;
-using PortalCT.Core.Interfaces;
-using PortalCT.Infrastructure.Data;
-using PortalCT.Infrastructure.Repositories;
-using PortalCT.Infrastructure.Services;
-using Microsoft.AspNetCore.Authentication.Cookies;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container
+// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
-// Database
-builder.Services.AddDbContext<PortalDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-// Authentication
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(options =>
-    {
-        options.LoginPath = "/Login";
-        options.LogoutPath = "/Logout";
-        options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
-        options.SlidingExpiration = true;
-    });
-
-builder.Services.AddAuthorization();
-
-// Repositories
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<IInformationRepository, InformationRepository>();
-
-// Services
-builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Services.AddScoped<ILegacyAuthService, LegacyAuthService>();
-
-// Add HttpClient for external services
-builder.Services.AddHttpClient();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
@@ -52,18 +18,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthentication();
-app.UseAuthorization();
-
 app.MapRazorPages();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
-
-// Ensure database is created
-using (var scope = app.Services.CreateScope())
-{
-    var context = scope.ServiceProvider.GetRequiredService<PortalDbContext>();
-    context.Database.EnsureCreated();
-}
 
 app.Run();
